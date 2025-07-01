09ce2b819667146fd3ce03434d0205f882c9e454
Venky <venkatramanm@gmail.com>
2025-07-01 23:17:07 +0530

    Test/Prod environment setting

--- src/main/java/in/succinct/bpp/core/db/model/User.java
+++ src/main/java/in/succinct/bpp/core/db/model/User.java
@@ -1,9 +1,12 @@
 package in.succinct.bpp.core.db.model;
 
 import com.venky.swf.db.Database;
+import com.venky.swf.db.annotations.column.COLUMN_DEF;
 import com.venky.swf.db.annotations.column.IS_NULLABLE;
 import com.venky.swf.db.annotations.column.IS_VIRTUAL;
 import com.venky.swf.db.annotations.column.UNIQUE_KEY;
+import com.venky.swf.db.annotations.column.defaulting.StandardDefault;
+import com.venky.swf.db.annotations.column.validations.Enumeration;
 
 import java.util.List;
 
@@ -12,6 +15,12 @@ public interface User extends com.venky.swf.plugins.collab.db.model.user.User {
     @UNIQUE_KEY(value = "PROVIDER_ID", allowMultipleRecordsWithNull = true)
     String getProviderId();
     void setProviderId(String providerId);
+    
+    @IS_NULLABLE(false)
+    @COLUMN_DEF(value = StandardDefault.SOME_VALUE,args = "test")
+    @Enumeration("test,production")
+    String getNetworkEnvironment();
+    void setNetworkEnvironment(String networkEnvironment);
     
     public static User findProvider(String providerId){
         User user  = Database.getTable(User.class).newRecord();
